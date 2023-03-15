f4be0ceda0e288249c7d6cb5c84f6cce35a3248e
thrallbata <vicortobdel@gmail.com>
2023-03-15 10:36:35 +0700

    add url and pattern menu

--- shop/models.py
+++ shop/models.py
@@ -7,21 +7,27 @@ class Category(models.Model):
     name = models.CharField(max_length=50, verbose_name='Название')
     slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
 
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'cat_id': self.pk})
+
 
 class Product(models.Model):
     name = models.CharField(max_length=255, verbose_name='Товар')
     slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
     photo = models.ImageField(upload_to="photo/%Y/%m/%d/")
     price = models.IntegerField()
-    specification = models.CharField(max_length=500)
-    warranty = models.CharField(max_length=4, verbose_name="Гарантия")
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    specification = models.CharField(max_length=2000)
+    warranty = models.CharField(max_length=15, verbose_name="Гарантия")
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return self.name
 
-    # def get_absolute_url(self):
-    #     return reverse('post', kwargs={'post_id': self.pk})
+    def get_absolute_url(self):
+        return reverse('product', kwargs={'prod_id': self.pk})
 
     # class Meta:
     #     verbose_name = 'Машины в наличии'
@@ -33,9 +39,6 @@ class Client(models.Model):
     name = models.CharField(max_length=50, verbose_name='Имя')
     phone_number = models.CharField(max_length=12, verbose_name='Номер телефона')
     email = models.EmailField(verbose_name='Почтовый адрес')
-
-    # def get_absolute_url(self):
-    #     return reverse('post', kwargs={'post_id': self.pk})
 
     # class Meta:
     #     verbose_name = 'Клиенты'
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,67 @@
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render
+
+from .models import *
+
+
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}]
 
 
 def index(request):
+    posts = Product.objects.all()
+    cats = Category.objects.all()
 
-    return render(request, 'shop/index.html')
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu,
+        'title': 'Главная страница',
+        'cat_selected': 0,
+    }
 
+    return render(request, 'shop/index.html', context=context)
+
+
+def about(request):
+    return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
+
+
+def addpage(request):
+    return HttpResponse("Добавление статьи")
+
+
+def contact(request):
+    return HttpResponse("Обратная связь")
+
+
+def login(request):
+    return HttpResponse("Авторизация")
+
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
+
+
+def show_post(request, prod_id):
+    return HttpResponse(f"Отображение статьи с id = {prod_id}")
+
+
+def show_category(request, cat_id):
+    products = Product.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+
+    if len(products) == 0:
+        raise Http404()
+
+    context = {
+        'posts': products,
+        'cats': cats,
+        'menu': menu,
+        'title': 'Отображение по рубрикам',
+        'cat_selected': cat_id,
+    }
+
+    return render(request, 'shop/index.html', context=context)
